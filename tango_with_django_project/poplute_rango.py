1374e74e5569254799d3c82cfcb38073b8185a4f
Kaiyuan Liu <kaiyuan.liu@livemobileco.com>
2014-11-13 00:49:42 +0000

    done with model and model templates

--- tango_with_django_project/poplute_rango.py
+++ tango_with_django_project/poplute_rango.py
@@ -9,8 +9,11 @@ django.setup()
 from rango.models import Category, Page
 
 
-def add_cate(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cate(name, views=0, likes=0):
+    c, created = Category.objects.get_or_create(name=name)
+    c.views = views
+    c.likes = likes
+    c.save()
     return c
 
 def add_page(cate, title, url, views=0):
@@ -19,34 +22,43 @@ def add_page(cate, title, url, views=0):
 
 def populate():
 
-    python_cate = add_cate('Python')
+    python_cate = add_cate('Python', views=128, likes=64)
+
     add_page(cate=python_cate, title='Official Python Tutorial',
-             url="http://docs.python.org/2/tutorial/")
+             url="http://docs.python.org/2/tutorial/",
+             views=4)
 
     add_page(cate=python_cate, title="How to think like a computer scientist",
-             url="http://www.greenteapress.com/thinkpython")
+             url="http://www.greenteapress.com/thinkpython",
+             views=10)
 
     add_page(cate=python_cate, title="Learn Python in 10 Minutes",
-             url="http://www.korokithakis.net/tutorials/python/")
+             url="http://www.korokithakis.net/tutorials/python/",
+             views=18)
 
-    django_cate = add_cate('Django')
+    django_cate = add_cate('Django', views=64, likes=32)
 
     add_page(cate=django_cate, title="Official Django Tutorial",
-             url="https://docs.djangoproject.com/en/1.7/intro/tutorial01/")
+             url="https://docs.djangoproject.com/en/1.7/intro/tutorial01/",
+             views=43)
 
     add_page(cate=django_cate, title="Django Rock",
-             url="http://www.djangorocks.com/")
+             url="http://www.djangorocks.com/",
+             views=11)
 
     add_page(cate=django_cate, title="How to tango with Django",
-             url="http://www.tangowithdjango.com/")
+             url="http://www.tangowithdjango.com/",
+             views=54)
 
-    framework_cate = add_cate('Other Frameworks')
+    framework_cate = add_cate('Other Frameworks', views=32, likes=16    )
 
     add_page(cate=framework_cate, title="Bottle",
-             url="http://bootlepy.org/docs/dev/")
+             url="http://bootlepy.org/docs/dev/",
+             views=77)
 
     add_page(cate=framework_cate, title='Flask',
-             url="http://flask.pocoo.org")
+             url="http://flask.pocoo.org",
+             views=83)
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 
 from .models import Category, Page
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
-admin.site.register(Category)
-admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
 
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
