18a4ff5649d214a7b7a84516247c19a34f53c340
Kaiyuan Liu <kaiyuan.liu@livemobileco.com>
2014-11-17 20:47:58 +0000

    refactory templates with bootstrap and jquery

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,4 +101,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #Login
 
-LOGIN_URL = '/rango/login/'
+#LOGIN_URL = '/rango/login/'
+
+#settings for django-registration-redux
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login/'
